d007d69c57a5a22711a934e5bc022583789e74f3
Thiago <thiagobarretocbv@hotmail.com>
2026-05-30 14:50:27 -0300

    added routers and login and main view

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { LoginComponent } from './components/view/login/login.component';
+import { AuthGuard } from './components/interceptors/auth.guard';
+import { AppMainViewComponent } from './components/view/login/app.main.view/app.main.view.component';
+
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', canActivate: [AuthGuard], component: AppMainViewComponent },
+    { path: 'login',  component: LoginComponent }
+]
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoaderComponent } from './components/interceptors/loader.component';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+    selector: 'app-root',
+    templateUrl: './app.html',
+    styleUrl: './app.scss',
+    imports: [
+        LoaderComponent,
+        RouterOutlet
+    ]
 })
 export class App {
-  protected readonly title = signal('NecroClock.Frontend');
+    protected readonly title = signal('NecroClock.Frontend');
 }
