b4cf6ec5a265ec69275d4cae94276ba203170b6d
Thiago <thiagobarretocbv@hotmail.com>
2026-05-30 14:30:18 -0300

    Add environments and nginx.conf

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+const api = `http://192.168.70.6:6004/api`;
+
+export const environment = {
+    production: false,
+
+    loginContext: `${api}/auth/`
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+const api = `http://localhost:8080/api`;
+
+export const environment = {
+    production: false,
+
+    loginContext: `${api}/auth/`
+};
